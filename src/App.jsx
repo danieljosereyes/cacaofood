a2e419556725294bd0ee4dfda53bd22aba674773
danieljosereyes <103333320+danieljosereyes@users.noreply.github.com>
2023-12-28 23:41:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import Section from './components/section'
+import Section from './components/Section'
 import Footer from './components/Footer'
 
 function App() {
