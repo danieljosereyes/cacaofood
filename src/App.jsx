19597ca18f067487b51d2b8363e9af56c2b77afd
danieljosereyes <danieljosereyes0000@gmail.com>
2023-12-29 20:22:23 -0400

    [ADD]  pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-// import { useState } from 'react'
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Main from "./pages/Main";
 import Footer from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    // <BrowserRouter>
-      <Routes>
-        <Route path="/cacaofood" element={<Main/>} />
-        <Route path="/cacaofood/about" element={<About/>} />
-      </Routes>
-
-    // </BrowserRouter>
+    <Routes>
+      <Route path="/cacaofood" element={<Main />} />
+      <Route path="/cacaofood/about" element={<About />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import Header from "./components/Header.jsx";
-import Menu from "./components/Menu.jsx";
+import Paginas from "./components/Paginas.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
-      <Menu />
+      <Paginas />
       <App />
     </BrowserRouter>
   </React.StrictMode>
